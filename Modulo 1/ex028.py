cbf5ac9abe4e5c9106a759bea11d5a7056e57ef5
Lucas Sampaio <lucas8221sampaio@gmail.com>
2026-07-01 23:18:07 -0300

    mais exercícios

---
+++ Modulo 1/ex028.py
@@ -0,0 +1,13 @@
+#Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+from random import randint
+
+computador = randint(0,5)
+
+print("Vou pensar em um numero entre 0 e 5. Tente adivinhar...")
+jogador = int(input("Em que numero eu pensei? "))
+
+if jogador == computador:
+    print("Parabéns! Você acertou!")
+else:
+    print("Ganhei! Eu pensei no numero {}".format(computador))
